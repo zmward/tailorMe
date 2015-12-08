e7307f1db4bebabaa9aca4958b4ce90f7acc7608
Peter <pmercado@ucsc.edu>
2015-12-07 19:15:23 -0800

    Update default.py

--- controllers/default.py
+++ controllers/default.py
@@ -1,145 +1,136 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
 
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+#########################################################################
+
+
+
 def index():
-    """
-    Allows a person to register in the system, if they are not registered already.
-    """
-    # If the person is registered, we store the person id in session.person_id.
-    db.people.name.label = "What's your name?"
-    logger.info("Session: %r" % session)
-    row = db(db.people.user_id == auth.user_id).select().first()
-    db.people.user_id.readable = db.people.user_id.writable = False
-    form = SQLFORM(db.people, record=row)
-    if form.process().accepted:
-        session.flash = "Welcome, %s!" % form.vars.name
-        redirect(URL('default', 'people'))
-
-    u_button = A(' Create New User', _class='btn btn-success fa fa-plus',
-                                             _href=URL('default', 'new_user'))
-
-    Measure_button = A(' Create New Mess', _class='btn btn-success fa fa-plus',
+    store_list = db(db.store).select()
+    add_button = A(' Create New Store', _class='btn btn-success fa fa-plus',
+                                             _href=URL('default', 'add_store'))
+    fit_button =  A(' Create New Fit', _class='btn btn-info fa fa-plus',
                                              _href=URL('default', 'tailor_you'))
-
-    Store_button = A(' Create New Store', _class='btn btn-success fa fa-plus',
-                                             _href=URL('default', 'Store'))
-
-    Review_button = A(' Create New Store', _class='btn btn-success fa fa-plus',
-                                             _href=URL('default', 'User_review'))
-
-    return dict(form=form, u_button=u_button, Measure_button=Measure_button, Store_button=Store_button,
-                Review_button=Review_button)
-
-@auth.requires_login()
-def people():
-    """
-    Gives the person a table displaying all the people, to search.
-    """
-    db.people.name.label = "Name"
-    # Creates a list of other people, other than myself.
-    q = (db.people.id != auth.user_id)
-    links = [dict(header='Click to chat',
-                 body = lambda r: A(I(_class='fa fa-comments'), 'Chat', _class='btn btn-success',
-                                    _href=URL('default', 'chat', args=[r.user_id])))]
-    grid = SQLFORM.grid(q,
-                        links=links,
-                        editable=False,
-                        details=False,
-                        csv=False)
-    return dict(grid=grid)
+    return dict(store_list=store_list, fit_button=fit_button, add_button=add_button)
 
 
-def store_message(form):
-    form.vars.msg_id = str(db2.textblob.insert(mytext = form.vars.msg_id))
-
-
-@auth.requires_login()
-def chat():
-    """This page enables you to chat with another person."""
-    # Let us read the record telling us who is the other person.
-    other = db(db.people.user_id == request.args(0)).select().first()
-    logger.info("I am %r, chatting with %r" % (auth.user_id, other))
-    if other is None:
-        # Back to square 0.
-        return redirect(URL('default', 'index'))
-    # Pair of people involved.
-    two_people = [auth.user_id, other.id]
-    # We want them in order, so that all messages will be stored under the same pairs of ids.
-    two_people.sort()
-    # This query selects all messages between the two people.
-    q = ((db.messages.user0 == two_people[0]) & (db.messages.user1 == two_people[1]))
-    # This is the list of messages.
-    db.messages.sender.represent = lambda v, r: 'You' if v == auth.user_id else other.name
-    grid = SQLFORM.grid(q,
-                        fields=[db.messages.msg_time, db.messages.sender, db.messages.msg_id],
-                        details=False,
-                        create=False,
-                        orderby=~db.messages.msg_time,
-                        csv=False,
-                        sortable=False,
-                        editable=False,
-                        deletable=False,
-                        searchable=False,
-                        user_signature=False)
-
-    # This is a form for adding one more message.
-
-    db.messages.sender.readable = db.messages.sender.writable = False
-    db.messages.msg_time.readable = db.messages.msg_time.writable = False
-    form = SQLFORM(db.messages)
-    form.vars.user0 = two_people[0]
-    form.vars.user1 = two_people[1]
-    if form.process(onvalidation=store_message).accepted:
-        session.flash = "Message sent!"
-        redirect(URL('default', 'chat', args=[other.user_id]))
-
-    title = "Chat with %s" % other.name
-    return dict(title=title, grid=grid, form=form)
-
-def new_user():
-    form = SQLFORM(db.profile)
-    if form.vars.accepted:
-        session.flash = "Welcome new User!"
-        redirect(URL('default', 'user'))
-
-    return dict(form=form)
+#--------------------------------------------
+# Adding a measurement table to the Profile
+#-------------------------------------------
 
 def tailor_you():
-    form = SQLFORM(db.Measure)
-    if form.vars.accepted:
-        session.flash = "Thank you!"
-        redirect(URL('default', index()))
+    Measure_list = db(db.Measure2).select()
+    add_button = A(' Fit Me!', _class='btn btn-success fa fa-plus', _href=URL('default', 'fit_me'))
 
-    return dict(form=form)
+    return dict(Measure_list=Measure_list, add_button=add_button)
+
+
+def fit_me():
+    form = SQLFORM(db.Measure2)
+    if form.process().accepted:
+        session.flash=T("Success!")
+        redirect(URL('tailor_you'))
+
+    back_button = A(' Back', _class='btn bth-info fa fa-reply', _href=URL('default', 'tailor_you'))
+
+    return dict(form=form, back_button=back_button)
+
+def check_complete(form):
+    if form.vars.height == None:
+        form.errors.height = T('Please Enter a Title for your Board')
+
+
+#--------------------------------
+# Creating a store and a review
+#--------------------------------
 
 def add_store():
     form = SQLFORM(db.store)
-    if form.vars.accepted:
-        session.flash = "Thank you!"
-        redirect(URL('default', 'Stores'))
+    if form.process(onvalidation=check_completea).accepted:
+        session.flash=T("Success!")
+        redirect(URL('show_store'))
 
-    return dict(form=form)
-    
-def Stores():
+    back_button = A(' Back', _class='btn bth-info fa fa-reply', _href=URL('default', 'show_store'))
+
+    return dict(form=form, back_button=back_button)
+
+def show_store():
     store_list = db(db.store).select()
-
-    add_button=A(' Create New Store', _class='btn btn-success fa fa-plus',
+    add_button = A(' Create New Store', _class='btn btn-success fa fa-plus',
                                              _href=URL('default', 'add_store'))
-
-    return dict(store_list=store_list, add_button=add_button)    
-
-
-def User_review():
-    form = SQLFORM(db.User_review)
-    if form.vars.accepted:
-        session.flash = "Thank you!"
-        redirect(URL('default', index()))
-
-    return dict(form=form)
+    fit_button =  A(' Create New Fit', _class='btn btn-info fa fa-plus',
+                                             _href=URL('default', 'tailor_you'))
+    return dict(store_list=store_list, fit_button=fit_button, add_button=add_button)
 
 
+def check_completea(form):
+    if form.vars.store_name == None:
+        form.errors.store_name = T('Please Enter a Title for your Board')
+
+
+def store_details():
+    store = db.store(request.args(0))
+    name = request.args(1)
+    if store is None:
+        session.flash = T('This Store does not Exist!')
+        redirect(URL('default', 'show_boards'))
+    form = SQLFORM(db.store, record=store, readonly=True)
+    list_button = A(' Back to Boards', _class='btn btn-info fa fa-reply',
+                    _href=URL('default', 'show_store'))
+    review = db(db.review.store_id == store.id).select()
+    add_button = A(' Create New Post', _class='btn btn-success fa fa-plus',
+                                             _href=URL('default', 'add_review', args=[store.id]))
+    return dict(form=form, review=review, list_button=list_button, add_button=add_button, name=name)
+
+def add_review():
+    store = db.store(request.args(0))
+    if store is None:
+        session.flash = T("There is no such Store")
+        redirct(URL('default', 'index'))
+
+    form = SQLFORM(db.review)
+    form.vars.store_id = store.id
+
+    if form.process().accepted:
+        form.vars.store_id = store.id
+        session.flash = T("You review has been created!")
+        redirect(URL('default', 'store_details', args=[store.id]))
+
+    back_button = A(' Cancel', _class='btn btn-warning fa fa-times',
+                    _href=URL('default', 'store_details', args=[store.id]))
+
+    return dict(form=form, back_button=back_button)
+
+
+
+#------------
+
+
+
+
+
+def delete_post():
+    db(db.post.id == request.args(0)).delete()
+    board = db.board(request.args(1))
+    session.flash = "Deleted"
+    redirect(URL('default', 'board_details', args=[board.id]))
+
+def delete_board():
+    db(db.board.id == request.args(0)).delete()
+    session.flash = "Board Deleted"
+    redirect(URL('default', 'index'))
+#------------
+# this is being usd to clear all tables then editing them
+#------------
 def reset():
-    db(db.profile.id > 0).delete()
+    db(db.board.id > 0).delete()
+    db(db.post.id > 0).delete()
+    db(db.store.id > 0).delete()
     db(db.Measure.id > 0).delete()
     db(db.height_table.id > 0).delete()
     db(db.head_table.id > 0).delete()
@@ -156,9 +147,8 @@ def reset():
     db(db.thigh_table.id > 0).delete()
     db(db.calf_table.id > 0).delete()
     db(db.ankle_table.id > 0).delete()
-    db(db.messages.id > 0).delete()
-    db(db.Store.id > 0).delete()
-    db(db.User_review.id > 0).delete()
+    db(db.review.id > 0).delete()
+
     redirect(URL('default', 'index'))
 
 
