d2972ab1df48a5fe8ad1f342be7836b9c5b890f8
Kelly Zhao <kllyzhao@gmail.com>
2015-12-09 15:31:14 -0800

    A TON of changes to HTML and CSS

--- controllers/default.py
+++ controllers/default.py
@@ -1,20 +1,223 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
 
+
+@auth.requires_signature()
+@auth.requires_login()
+def profile():
+    measure_form = SQLFORM(db.Measure2)
+    if measure_form.vars.accepted:
+        session.flash = "Thank you!"
+        redirect(URL('default', index()))
+
+    return dict(measure_form=measure_form)
+
+
+
 def index():
-    """
-    Allows a person to register in the system, if they are not registered already.
-    """
-    # If the person is registered, we store the person id in session.person_id.
-    db.people.name.label = "What's your name?"
-    logger.info("Session: %r" % session)
-    row = db(db.people.user_id == auth.user_id).select().first()
-    db.people.user_id.readable = db.people.user_id.writable = False
-    form = SQLFORM(db.people, record=row)
+    store_list = db(db.store).select()
+    add_button = A(' Create New Store', _class='btn btn-success fa fa-plus',
+                                             _href=URL('default', 'add_store'))
+    fit_button =  A(' Create New Fit', _class='btn btn-info fa fa-plus',
+                                             _href=URL('default', 'tailor_you'))
+    return dict(store_list=store_list, fit_button=fit_button, add_button=add_button)
+
+
+#--------------------------------------------
+# Adding a measurement table to the Profile
+#-------------------------------------------
+
+def tailor_you():
+    Measure_list = db(db.Measure2).select()
+    add_button = A(' Fit Me!', _class='btn btn-success fa fa-plus', _href=URL('default', 'fit_me'))
+
+    return dict(Measure_list=Measure_list, add_button=add_button)
+
+
+def fit_me():
+    form = SQLFORM(db.Measure2)
     if form.process().accepted:
-        session.flash = "Welcome, %s!" % form.vars.name
-        redirect(URL('default', 'people'))
-    return dict(form=form)
+        session.flash=T("Success!")
+        redirect(URL('tailor_you'))
+
+    back_button = A(' Back', _class='btn bth-info fa fa-reply', _href=URL('default', 'tailor_you'))
+
+    return dict(form=form, back_button=back_button)
+
+def check_complete(form):
+    if form.vars.height == None:
+        form.errors.height = T('Please Enter a Title for your Board')
+
+
+#--------------------------------
+# Creating a store and a review
+#--------------------------------
+
+def add_store(): #was add_store
+    form = SQLFORM(db.store)
+    if form.process(onvalidation=check_completea).accepted:
+        session.flash=T("Success!")
+        redirect(URL('show_store'))
+
+    back_button = A(' Back', _class='btn bth-info fa fa-reply', _href=URL('default', 'show_store'))
+
+    return dict(form=form, back_button=back_button)
+
+def show_store():
+    store_list = db(db.store).select()
+    add_button = A(' Create New Store', _class='btn btn-success fa fa-plus',
+                                             _href=URL('default', 'add_store'))
+    fit_button =  A(' Create New Fit', _class='btn btn-info fa fa-plus',
+                                             _href=URL('default', 'tailor_you'))
+    return dict(store_list=store_list, fit_button=fit_button, add_button=add_button)
+
+
+def check_completea(form):
+    if form.vars.store_name == None:
+        form.errors.store_name = T('Please Enter a Title for your Board')
+
+
+def store_details():
+    store = db.store(request.args(0))
+    name = request.args(1)
+    if store is None:
+        session.flash = T('This Store does not Exist!')
+        redirect(URL('default', 'show_boards'))
+    form = SQLFORM(db.store, record=store, readonly=True)
+    list_button = A(' Back to stores', _class='btn btn-primary btn-lg outline fa fa-reply',
+                    _href=URL('default', 'show_store'))
+    review = db(db.review.store_id == store.id).select()
+    add_button = A(' Add a review', _class='btn btn-primary btn-lg outline fa fa-plus',
+                                             _href=URL('default', 'add_review', args=[store.id]))
+    return dict(form=form, review=review, list_button=list_button, add_button=add_button, name=name)
+
+def add_review():
+    store = db.store(request.args(0))
+    if store is None:
+        session.flash = T("There is no such Store")
+        redirect(URL('default', 'index'))
+
+    form = SQLFORM(db.review)
+    form.vars.store_id = store.id
+
+    if form.process().accepted:
+        form.vars.store_id = store.id
+        session.flash = T("You review has been created!")
+        redirect(URL('default', 'store_details', args=[store.id]))
+
+    back_button = A(' Cancel', _class='btn btn-primary btn-lg outline fa fa-times',
+                    _href=URL('default', 'store_details', args=[store.id]))
+
+    return dict(form=form, back_button=back_button)
+
+def store_edit():
+    store = db.store(request.args(0))
+    if store is None:
+        session.flash = T('No such Store')
+        redirect(URL('default', 'show_store'))
+    form = SQLFORM(db.store, record=store)
+    if form.process(onvalidation=check_complete).accepted:
+        session.flash = T('The data was edited')
+        redirect(URL('default', 'store_details', args=[store.id]))
+    edit_button = A(' View', _class='btn btn-warning',
+                    _href=URL('default', 'store_details', args=[store.id]))
+    return dict(form=form, edit_button=edit_button)
+
+def review_edit():
+    review = db.review(request.args(0))
+    store = db.store(request.args(1))
+
+    if review is None:
+        session.flash = T('An error has Occurred')
+        redirect(URL('default', 'show_stores'))
+    form = SQLFORM(db.review, record=review)
+    if form.process(onvalidation=check_complete_post).accepted:
+        session.flash = T('The review was edited!')
+        redirect(URL('default', 'store_details', args=[store.id]))
+    back_button = A(' Back', _class='btn btn-warning fa fa-reply',
+                    _href=URL('default', 'store_details', args=[store.id]))
+    return dict(form=form, back_button=back_button)
+
+
+def delete_review():
+    db.review.id == request.args(0).delete()
+    store = db.store(request.args(1))
+    session.flash = "Deleted"
+    redirect(URL('default', 'store_details', args=[store.id]))
+
+def delete_store():
+    db(db.store.id == request.args(0)).delete()
+    session.flash = "Store Deleted"
+    redirect(URL('default', 'show_store'))
+#------------
+
+
+#------------
+# this is being usd to clear all tables then editing them
+#------------
+def reset():
+    db(db.board.id > 0).delete()
+    db(db.post.id > 0).delete()
+    db(db.store.id > 0).delete()
+    db(db.Measure.id > 0).delete()
+    db(db.height_table.id > 0).delete()
+    db(db.head_table.id > 0).delete()
+    db(db.neck_table.id > 0).delete()
+    db(db.chest_table.id > 0).delete()
+    db(db.waist_table.id > 0).delete()
+    db(db.sh_table.id > 0).delete()
+    db(db.hip_table.id > 0).delete()
+    db(db.wrist_table.id > 0).delete()
+    db(db.biceps_table.id > 0).delete()
+    db(db.forearm_table.id > 0).delete()
+    db(db.arm_table.id > 0).delete()
+    db(db.inseam_table.id > 0).delete()
+    db(db.thigh_table.id > 0).delete()
+    db(db.calf_table.id > 0).delete()
+    db(db.ankle_table.id > 0).delete()
+    db(db.review.id > 0).delete()
+
+    redirect(URL('default', 'index'))
+
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+
 
 @auth.requires_login()
 def people():
--- models/tables.py
+++ models/tables.py
@@ -51,3 +51,126 @@ def get_text(v):
 db2.define_table('textblob',
     Field('mytext', 'text'),
     )
+
+
+#------------------------------
+# Homework 3 stuff
+#------------------------------
+
+db.define_table( 'person',
+                 Field('user_id', db.auth_user),
+                 Field( 'name', required=True),
+                 )
+
+db.define_table('board',
+                 Field( 'board_author', db.auth_user, default=auth.user_id),
+                 Field('board_name', required=True),
+)
+db.board.id.readable = db.board.id.writable = False
+db.board.board_author.readable = db.board.board_author.writable=False
+
+
+db.define_table( 'post',
+                 Field('post_author', db.auth_user, default=auth.user_id),
+                 Field('board_id', 'reference board'),
+                 Field('title' ),
+                 Field('description', 'text'),
+)
+db.post.id.readable = db.post.id.writable = False
+db.post.post_author.readable = db.post.post_author.writable=False
+db.post.board_id.readable = db.post.board_id.writable=False
+
+db.post.board_id.on_delete = "SET NULL"
+
+
+#----------------------------------
+# Tailor me stuff
+
+db.define_table('store',
+                Field('store_author', db.auth_user, default=auth.user_id),
+                Field('store_name', required=True),
+)
+
+
+db.store.store_author.readable = db.store.store_author.writable = False
+db.store.id.readable = db.store.id.writable = False
+
+
+db.define_table('review',
+                Field('author', db.auth_user, default=auth.user_id ),
+                Field('store_id', 'reference store'),
+                Field('Tops', 'boolean', default=False),
+                Field('FT', label='Fit', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('CT', label='Cut', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('QT', label='Quality', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('Bottoms', 'boolean', default=False),
+                Field('FB', label='Fit', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('CB', label='Cut', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('QB', label='Quality', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('Dress', 'boolean', default=False),
+                Field('FD', label='Fit', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('CD', label='Cut', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('QD', label='Quality', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('Shoes', 'boolean', default=False),
+                Field('Outerwear', 'boolean', default=False),
+                Field('Intimates', 'boolean', default=False),
+
+                Field('Fit', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('Cut', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('Quality', requires=IS_IN_SET(['1', '2', '3', '4', '5']), default=3),
+                Field('review', 'text'),
+                Field('This_is_image', 'boolean', default=False),
+)
+
+
+db.review.store_id.on_delete = "SET NULL"
+db.review.store_id.readable = db.review.store_id.writable = False
+db.review.author.readable = db.review.author.writable = False
+
+#-------------------------
+# Testing messure tables
+#-------------------------
+
+
+
+
+
+
+db.define_table('Measure',
+                Field('author'),
+                Field('height', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('head', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('neck', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('chest', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('waist', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('shoulder', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('hip', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('wrist', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('biceps', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('forearm', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('arm_length', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('inseam', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('thigh', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('calf', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('ankle', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                )
+
+
+db.define_table('Measure2',
+                Field('author', db.auth_user, default=auth.user_id ),
+                Field('height', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('head', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('neck', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('chest', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('waist', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('shoulder', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('hip', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('wrist', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('biceps', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('forearm', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('arm_length', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('inseam', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('thigh', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('calf', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                Field('ankle', requires=IS_IN_SET(['petite', 'regular', 'tall', '4', '5', '6', '7'])),
+                )
