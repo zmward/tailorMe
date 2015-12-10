2f052f56a2ad247dc5b5300214b4744405a383e0
Zach <Aftermath1991@gmail.com>
2015-12-10 13:33:42 -0800

    negated SQLite errors

--- models/db.py
+++ models/db.py
@@ -17,11 +17,11 @@ myconf = AppConfig(reload=True)
 
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
-    db2 = DAL(myconf.take('db.uri2'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
+    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'], migrate_enabled=False)
+    db2 = DAL(myconf.take('db.uri2'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'], migrate_enabled=False)
 else:
     ## connect to Google BigTable (optional 'google:datastore://namespace')
-    db = DAL('google:datastore+ndb')
+    db = DAL('google:datastore+ndb', migrate_enabled=False)
     db2 = db
     ## store sessions and tickets there
     session.connect(request, response, db=db)
